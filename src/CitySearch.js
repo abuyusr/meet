2556dc42d7fe71c095ed98fee6407f6821d157cf
abuyusr <abdizm@outlook.com>
2023-06-02 19:30:45 -0400

    Applying Changes to existing features

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
   state = {
     query: '',
-    suggestions: []
+    suggestions: [],
+    showSuggestions: undefined
   }
 
   handleInputChanged = (event) => {
@@ -19,7 +20,8 @@ class CitySearch extends Component {
 
   handleItemClicked = (suggestion) => {
     this.setState({
-      query: suggestion
+      query: suggestion,
+      showSuggestions: false
     });
   
     this.props.updateEvents(suggestion);
@@ -31,25 +33,23 @@ class CitySearch extends Component {
     return (
       <div className="CitySearch">
         <input
-          type="text"
-          className="city"
-          value={this.state.query}
-          onChange={this.handleInputChanged}
+         type="text"
+         className="city"
+         value={this.state.query}
+         onChange={this.handleInputChanged}
+         onFocus={() => { this.setState({ showSuggestions: true }) }}
         />
-        <ul className="suggestions">
-  {this.state.suggestions.map((suggestion) => (
-    <li
-      key={suggestion}
-      onClick={() => this.handleItemClicked(suggestion)}
-    >{suggestion}</li>
-  ))}
-  <li onClick={() => this.handleItemClicked("all")}>
-  <b>See all cities</b>
-</li>
-
-</ul>
-
-
+        <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
+          {this.state.suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+               >{suggestion}</li>
+              ))}
+              <li onClick={() => this.handleItemClicked("all")}>
+              <b>See all cities</b>
+            </li>
+            </ul>
       </div>
     );
   }
--- src/tests/CitySearch.test.js
+++ src/tests/CitySearch.test.js
@@ -11,6 +11,12 @@ describe('<CitySearch /> component', () => {
     CitySearchWrapper = shallow(<CitySearch locations={locations} updateEvents={() => {}} />);
   });
 
+  test("selecting CitySearch input reveals the suggestions list", () => {
+    CitySearchWrapper.find('.city').simulate('focus');
+    expect(CitySearchWrapper.state('showSuggestions')).toBe(true);
+    expect(CitySearchWrapper.find('.suggestions').prop('style')).not.toEqual({ display: 'none' });
+  });
+
   test('renders text input correctly', () => {
     const CitySearchWrapper = shallow(<CitySearch />);
     const query = CitySearchWrapper.state('query');
@@ -62,6 +68,16 @@ describe('<CitySearch /> component', () => {
     const suggestions = CitySearchWrapper.state('suggestions');
     CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
     expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
+  });
+
+  test("selecting a suggestion should hide the suggestions list", () => {
+    CitySearchWrapper.setState({
+      query: 'Berlin',
+      showSuggestions: undefined
+    });
+    CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+    expect(CitySearchWrapper.state('showSuggestions')).toBe(false);
+    expect(CitySearchWrapper.find('.suggestions').prop('style')).toEqual({ display: 'none' });
   });
 
 });
